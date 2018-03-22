fff1d82283d88e3fa7644988f7617316a9541fa9
Ingenieria Sistemas19 <isi19@labs.uca.edu.ni>
2018-03-22 17:28:29 -0600

    version1

---
+++ app/src/main/java/com/uca/alex/cesar/fajardo/sqlite333/AdminSQLiteOpenHelper.java
@@ -0,0 +1,25 @@
+package com.uca.alex.cesar.fajardo.sqlite333;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class AdminSQLiteOpenHelper extends SQLiteOpenHelper {
+
+    public AdminSQLiteOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table votantes(dni integer primary key, nombre text, colegio text, nromesa integer)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int versionAnterior, int versionPosterior) {
+        db.execSQL("drop table if exists votantes");
+        db.execSQL("create table votantes(dni integer primary key, nombre text, colegio text, nromesa integer)");
+    }
+
+
+}
